9fef1b0cc31afce632c7ff9509ea8d4db46d514c
XQ612DJ <XQ612DJ@IN3086658W1.ey.net>
2024-04-28 12:38:27 +0530

    VT Main menu

---
+++ Test Suites/TS_VT_MainMenu.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_VT_MainMenu</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e301b887-265b-44ca-b1e0-3306c3ce71d2</testSuiteGuid>
+   <testCaseLink>
+      <guid>92552adc-7011-465c-8dd8-d62324e4c707</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VT_MainMenu</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
