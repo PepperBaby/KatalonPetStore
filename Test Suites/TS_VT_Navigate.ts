ee458175d686eb98e3336bcbcc66289f5f7dadfc
XQ612DJ <XQ612DJ@IN3086658W1.ey.net>
2024-04-28 12:17:59 +0530

    Visual Testing Navigate

---
+++ Test Suites/TS_VT_Navigate.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_VT_Navigate</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0da2fb30-f434-4984-9b62-4066ca5ecc99</testSuiteGuid>
+   <testCaseLink>
+      <guid>096e8bfb-2454-4f25-8ac6-1222f5712753</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VT_Navigate</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
